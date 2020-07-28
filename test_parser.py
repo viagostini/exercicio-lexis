6eb7596f956d3cd9b141893fa6b69e213ee794ba
Vinicius Agostini <vinicius.agostini19@gmail.com>
2020-07-28 10:49:23 -0300

    fixing test name

--- test_parser.py
+++ test_parser.py
@@ -4,7 +4,7 @@ from parser import Parser
 
 
 class TestParser(unittest.TestCase):
-    def test_file_reader(self):
+    def test_read_file(self):
         people = Parser.read_file("data.csv")
         assert len(people) == 4
         assert people["outroemail@hotmail.com"]["first_name"] == "vinicius"
@@ -12,7 +12,7 @@ class TestParser(unittest.TestCase):
         assert people["outroemail@hotmail.com"]["birth"] == "19960603"
         assert people["outroemail@hotmail.com"]["gender"] == "masculino"
 
-    def test_file_reader_raises_on_wrong_extension(self):
+    def test_read_file_raises_on_wrong_extension(self):
         self.assertRaises(RuntimeError, Parser.read_file, "data.tsv")
 
 
